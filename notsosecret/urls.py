d3ebcdf3641253aaa63c9639c54ae8fd7ae8785f
vagrant <vagrant@precise64.(none)>
2014-05-10 01:33:24 +0000

    Nope.

--- notsosecret/urls.py
+++ notsosecret/urls.py
@@ -8,6 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'notsosecret.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
     url(r'^$', include('secretkeeper.urls')),
 )
